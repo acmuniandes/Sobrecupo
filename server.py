4edbe1880e7ac584ef628a8156eb806fe54ef0d0
Julián <ja.manrique@uniandes.edu.co>
2017-07-11 17:23:51 -0500

    index and protocol fixes

--- server.py
+++ server.py
@@ -20,9 +20,17 @@ def saveInDB(strng):
     r.lpush('db', '%s' % strng)
     return 'OK'
 
+#Recalls the stored info in the DB
+@app.route('/db/recall/test', methods = ['GET'])
+def recallDB():
+    try:
+        return str(r.lrange('db', 0, -1))
+    except Exception as error:
+        return formatError(error)
+
 #DevMode Handling------------------------------------
 
-#Returns .js file (Vue)
+#Returns devMode.html [GET] #Checks login credentials [POST]
 @app.route('/devMode', methods = ['POST', 'GET'])
 def devMode():
 
@@ -34,22 +42,19 @@ def devMode():
         if checkPassword(pss['password'], request.environ['REMOTE_ADDR']):
             return os.environ.get('PASSWORD_MESSAGE')
         else:
-            return 'quePasoAmiguitoxdxd ' + pss['password']
+            return 'quePasoAmiguitoxdxd'
     else:
         return '¿?'
 
-#Recalls the stored info of the DB
-@app.route('/db/recall', methods = ['POST'])
+#Recalls the failed log attemps of devMode
+@app.route('/devMode/recall/logAttempts', methods = ['POST'])
 def recallDB():
     pss = request.get_json() or request.form
     if checkPassword(pss['password'], request.environ['REMOTE_ADDR']):
         try:
-            return str(r.lrange('db', 0, -1))
+            return str(r.lrange('devModeGetList', 0, -1))
         except Exception as error:
-            msg = ''
-            msg += 'Type : ' + type(error) + '\n'
-            msg += 'Exception: ' + error
-            return msg
+            return formatError(error)
     else:
         return 'la pulenta oe'
 
@@ -60,4 +65,9 @@ def checkPassword(password, ip):
     if not attempt:
         r.lpush('devModeGetList', "*IP: " + ip + " // " + "Timestamp: " + date + " // Attempt: " + password + "\n")
     return attempt
-        
+
+#Format function to redis errors
+def formatError(error):
+    msg = 'Type: ' + type(error) + '\n'
+    msg += 'Exception: ' + error
+    return msg
