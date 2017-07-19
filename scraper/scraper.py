07348ae5a753beab8d95e30db92042f468125df9
Julián <ja.manrique@uniandes.edu.co>
2017-07-19 01:12:34 -0500

    Network workaround + added log + started real-case scrape

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,12 +4,14 @@ from bs4 import BeautifulSoup
 
 #Constants---------------------
 
-BASE_URL = "https://registroapps.uniandes.edu.co/"
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+BASE_URL = "https://registroapps.uniandes.edu.co"
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1003.1 Safari/535.19 Awesomium/1.7.4.2'
 ACCEPT_LANGUAGE = "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3"
 EXTRA_SCHEDULE = 26
 MARKER = 'marked'
 MARKED = 'True'
+LOG_SEPARATOR = '---------------------------------------------'
+LOG_FREQUENCY = 2400
 
 #Global variables--------------
 
@@ -38,39 +40,37 @@ class _Class:
         self.teacher = None
         self.schedules = None
         #Debugging
-        print(name)
+        #print(name)
     
     def completeInfo(self, schedules, teacher):
         self.teacher = teacher
         self.schedules = schedules
         #Debugging
-        for schedule in schedules:
-            print("  " + debugSchedule(schedule))
+        #for schedule in schedules:
+        #    print("  " + debugSchedule(schedule))
 
 #Methods-----------------------
 
 def scrape():
 
-    mainPage = request("https://registroapps.uniandes.edu.co/scripts/adm_con_horario_joomla.php")
-    mainPage = request("https://registroapps.uniandes.edu.co/scripts/semestre/adm_con_horario1_joomla.php?depto=ANTR&nombreDepto=Antropolog%EDa")
-
-    print(mainPage)
+    mainPage = None
+    with open('indexReg.html', 'r') as file:
+        mainPage = file.read()
 
     depSoup = BeautifulSoup(mainPage, 'html5lib')
 
     for link in depSoup.find_all('a', href=True):
-        #scrapeDep(link)
-        print(link.string)
+        scrapeDep(link)
 
 def scrapeDep(depTag):
 
     #Stores the url from the tag
-    depHTML = depTag['href']
+    depHTML = depTag['href'][37:-1]
 
     print("Entering: " + depHTML)
 
     if isRelative(depHTML):
-        depHTML = BASE_URL + depHTML[2:-1]
+        depHTML = BASE_URL + depHTML
 
     depPage = request(depHTML)
 
@@ -91,7 +91,8 @@ def extractTables(tableTag):
     #Check if it is a schedules section
     schedules = matchSchedule(descendants)
     if schedules:
-        #TODO Test current schedule scraping
+        #TODO Fix for classes with 3 or more teachers: use boolean iteration
+        #   -Table structure doesn't change, so, it's not caused by teacher quantity
         
         #Adds schedules and teacher's name to currentClass pointer
         iterateSchedules(tableTag, schedules)
@@ -99,6 +100,7 @@ def extractTables(tableTag):
 
         if validateClass(classPointer):
             classList.append(classPointer)
+            logProcess(classPointer)
             print("----" + classPointer.name + " added----")
         else:
             print("----" + classPointer.name + " dismissed----")
@@ -106,7 +108,7 @@ def extractTables(tableTag):
         classPointer = None        
         
     elif descendants >= 77 and descendants < 370:
-        #TODO Finish _Class scraping and serializing
+        #TODO Prettify/document next 8 lines
         try:
             classPointer = _Class(tableTag.find_all(encontrarTitulo)[0].string.strip())
         except Exception as error:
@@ -123,8 +125,8 @@ def extractTables(tableTag):
 def matchSchedule(numOfDescendants):
     fitsSchedule = False
     schedules = 0
-    #TODO Check if there are courses with more than 8 different schedules
-    for c in range(9):
+    #TODO Check if there are courses with more than 9 different schedules
+    for c in range(10):
         fitsSchedule = numOfDescendants == 83 + c*EXTRA_SCHEDULE
         if fitsSchedule:
             schedules = c + 1
@@ -150,9 +152,12 @@ def iterateSchedules(parentTag, schedules):
         #Extracts it's info into an Schedule object and adds it to the list
         scheduleList.append(extractSchedule(actualTag))
 
-    #Extracts current class' teacher name
+    #Extracts current class' teacher(s) name
     actualTag = scheduleTRs[schedules]
     teacher = actualTag.find('td', width='172').string.strip()
+
+    extra = actualTag.find_all('td')
+    print (str(len(extra)))
 
     #Finally, add info to current class pointer
     classPointer.completeInfo(scheduleList, teacher)
@@ -173,29 +178,29 @@ def extractSchedule(tag):
 #Auxiliary methods-------------
 
 def isRelative(url):
-    return url.startswith('..')
+    return url.startswith('/')
 
 def request(url):
 
     s = requests.Session()
 
     custom_headers = {
-        'user-agent' : USER_AGENT,
-        'accept': "text/html;charset=UTF-8",
-        'accept_language': ACCEPT_LANGUAGE
+        'User-Agent' : USER_AGENT,
+        'Accept': "text/html;charset=UTF-8",
+        'Accept-Language': ACCEPT_LANGUAGE,
+        'Cookie': '__utma=154978747.1072089661.1499469016.1500390322.1500417611.7; __utmz=154978747.1500417611.7.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); _ga=GA1.3.1072089661.1499469016; _gid=GA1.3.1963782703.1500382504; __utmb=154978747.5.10.1500417611; _gat=1; __utmc=154978747'
     }
     #response = requests.get(url, headers=custom_headers)
 
     jar = requests.cookies.RequestsCookieJar()
     jar.set('_utma', '154978747.1072089661.1499469016.1500386404.1500390322.6')
-    jar.set('__utmz', "154978747.1499469016.1.1.utmctmccn=(direct)|utmcmd=(none)")
+    jar.set('__utmz', "154978747.1023459016.1.1.utmctmccn=(direct)|utmcmd=(none)")
     jar.set('_ga', "GA1.3.1072089661.1499469016")
     jar.set('_gid', "GA1.3.1963782703.1500382504")
     jar.set('__utmc', "154978747")
 
-    response = s.get(url, cookies=jar, headers=custom_headers, stream=True, verify=True)
+    response = s.get(url, headers=custom_headers)
     response.encoding = "utf-8"
-    print(str(response.headers))
     return response.text
 
 def debugSchedule(sch):
@@ -209,17 +214,29 @@ def validateClass(clss):
         valid = valid and len(clss.schedules[0].weekdays) >0
     return valid
 
+processString = ""
+def logProcess(clss):
+    global processString
+    string = "\n" + LOG_SEPARATOR + "\n" + clss.name 
+    for schedule in clss.schedules:
+        string += "\n   " + debugSchedule(schedule)
+    processString += string
+    if len(processString) > LOG_FREQUENCY:
+        log(processString)
+        processString = ""
 
+def log(strng):
+    with open('Log.log', 'a') as file:
+        file.write(strng)
 #Tag recognition Methods(bs4)--
 
 #Accepts tags that aren't marked
 def detectScheduleTR(tag):
-    #print("From " + str(tag.name) + "// A = " + str(tag.attrs) + " //Tag get = " + str(tag.get(MARKER)))
     return tag.name == "tr" and tag.get(MARKER) is None
 
 
 def encontrarTitulo(tag):
-    return tag.has_attr('width') and tag['width'] == "201" and " " in tag.string
+    return tag.has_attr('width') and tag['width'] == "156" and " " in tag.string
 
 #Excecution
 scrape()
