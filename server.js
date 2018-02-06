945d4f8eccf98f34c8b3d2f771fb7d2be307abc0
JaManrique <ja.manrique@uniandes.edu.co>
2018-02-06 14:25:04 -0500

    Server structure added. Old functions still do not work

--- server.js
+++ server.js
@@ -2,12 +2,23 @@
 
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/web/sobrecupo-web/dist"));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('server started '+ port);
+console.log('server started: '+ port);
+
+
+app.get('/salones', (request, response) => {
+    fs.readFile('classrooms.json', 'utf8', (error, data) => {
+        if(error){
+            console.error(error);
+        }
+        response.send(data);
+    })
+});
--- web/sobrecupo-web/src/components/ClassroomList.vue
+++ web/sobrecupo-web/src/components/ClassroomList.vue
@@ -15,13 +15,15 @@ export default {
   data() {
     return {
       info: null,
-      error: ''
+      error: "",
+      freeClassrooms: []
     };
   },
   mounted() {
     //Using a pointer to this due to the context change on axios function
     const _this = this;
-    axios.get("/salones")
+    axios
+      .get("/salones")
       .then(response => {
         _this.info = response.data;
       })
@@ -29,153 +31,224 @@ export default {
         console.log(error);
         _this.error = error.data;
       });
-    
+
     this.classroomNow(new Date());
     console.log(this.info);
   },
-  methods:{
-    classroomNow: function(date){
+  methods: {
+    generateDate: function(date) {
+      const day = date.getDate();
+      let month = date.getMonth() + 1;
+
+      //Month format for json: if month beofre october: 0X, others XX
+      if (month < 10) month = "0" + month;
+
+      return day + "/" + month + "/18";
+    },
+
+    //@TODO Correct empty interval-emptiness
+    //(technically all classrooms are free between classes)
+    isInCountinuityTuple: function(hour, tuple) {
+      //A continuity tuple is defined as a continous time interval in which the classroom is occupied.
+      //The method just checks if the given hour is between the boundaries of the given tuple
+
+      //tuple[0] is the lower boundary
+      //tuple[1] is the uppper boundary
+
+      //If the hour is bigger than the lower boundary
+      if (
+        hour >= parseInt(tuple[0]) /*plus/minus the dead time interval*/ - 10
+      ) {
+        //and is lower than the upper boundary
+        if (hour < parseInt(tuple[1])) {
+          //Then, it means that it is within the boundaries
+          return true;
+        }
+      }
+      return false;
+    },
+
+    formatFreeClassroom: function(classroom) {
+      let msg = "";
+      msg += classroom.id.slice(1);
+
+      if (typeof classroom.TUO == "string")
+        msg += ": Está libre hasta el final del dia";
+      else if (classroom.TUO > 60)
+        msg +=
+          ": Está libre durante " +
+          Math.floor(classroom.TUO / 60) +
+          "h " +
+          classroom.TUO % 60 +
+          "m  [" +
+          classroom.TUO +
+          "m]";
+      else msg += ": Está libre durante " + classroom.TUO + " minutos";
+
+      return msg;
+    },
+
+    randomClassroom: function() {
+      if (this.freeClassrooms.length == 0) {
+        this.freeClassroom = "Paila";
+        return this.freeClassroom;
+      } else {
+        let number = Math.random() * (this.freeClassrooms.length - 1);
+        number = Math.round(number);
+        const chosen = this.freeClassrooms[number];
+
+        this.freeClassroom = this.formatFreeClassroom(chosen);
+        return this.freeClassroom;
+      }
+    },
+
+    arrContains: function(array, element) {
+      for (i in array) if (array[i] == element) return true;
+      return false;
+    },
+
+    extractBuilding: function(classroom) {
+      str = classroom.split("_")[0];
+      return str.slice(1);
+    },
+
+    classroomNow: function(date) {
       //First part of the method generates day schedule from base json
-      classrooms = this.info
-      exceptions = []
-      localClassrooms = {}
-      today = this.generateDate(date)
-      for(classroom in classrooms)
-      {
+      const classrooms = this.info;
+      let exceptions = [];
+      let localClassrooms = {};
+      let today = this.generateDate(date);
+      for (classroom in classrooms) {
         //If the classroom does not belong to the known invalid classrooms list, compute it
-        if(this.invalidClassrooms.indexOf(classroom) == -1)
-        {
-          schedule = []
-          exceptions = classrooms[classroom]["exceptions"][today]
+        if (this.invalidClassrooms.indexOf(classroom) == -1) {
+          let schedule = [];
+          exceptions = classrooms[classroom]["exceptions"][today];
 
           /*
           console.log("Exceptions " + classroom + " // " + today + ":")
           console.log(exceptions)
           */
 
-          if(exceptions != undefined)
-          {
+          if (exceptions != undefined) {
             //The 'today' field contains a string with the format "SCH$$SCH$$ (...)"
             //TODO error may be on month/day format (AGO = 08 and not 8, etc.)
-            schedule = schedule.concat(exceptions.split("$$"))
+            schedule = schedule.concat(exceptions.split("$$"));
 
             //console.log("after exp: " + schedule)
           }
           //This line converts to a JS object but the string is not optimally formed, so it has to replace
           // ' ocurrences with " to be a valid json to parse
-          base = JSON.parse(classrooms[classroom][(date.getDay()-1).toString()].replace(/\'/g, '\"'))
+          let base = JSON.parse(
+            classrooms[classroom][(date.getDay() - 1).toString()].replace(
+              /\'/g,
+              '"'
+            )
+          );
 
-          for(sch in base)
-            schedule.push(base[sch])
-          
-          localClassrooms[classroom] = schedule
+          for (sch in base) schedule.push(base[sch]);
+
+          localClassrooms[classroom] = schedule;
         }
       }
 
       //Second part, finds free classrooms from previous data structure formed
 
-      console.log("Local classrooms: ")
-      console.log(localClassrooms)
+      console.log("Local classrooms: ");
+      console.log(localClassrooms);
 
-      hour = date.getHours()*100 + date.getMinutes()
+      let hour = date.getHours() * 100 + date.getMinutes();
 
-      for(classroom in localClassrooms)
-      {
-          //Warden variable, modeling that the hour belongs to the tuple
-          warden = true
+      for (classroom in localClassrooms) {
+        //Warden variable, modeling that the hour belongs to the tuple
+        warden = true;
 
-          //Variable that saves the shortest (but positive) difference betweeen
-          //the first element of all cointinuity tuples
-          timeUntilOccupation = 9000
+        //Variable that saves the shortest (but positive) difference betweeen
+        //the first element of all cointinuity tuples
+        timeUntilOccupation = 9000;
 
-          //Variables that store information to fix base 10 aritmethic offset (later explained)
-          hourI = 0
-          hourF = 0
+        //Variables that store information to fix base 10 aritmethic offset (later explained)
+        hourI = 0;
+        hourF = 0;
 
-          for(continuityTuple in localClassrooms[classroom])
-          {
-            continuity_tuple = localClassrooms[classroom][continuityTuple].split(/ - /g)
+        for (continuityTuple in localClassrooms[classroom]) {
+          continuity_tuple = localClassrooms[classroom][continuityTuple].split(
+            / - /g
+          );
 
-            //console.log("Non split: " + localClassrooms[classroom][continuityTuple])
-            //console.log("Splitted: " + continuity_tuple)
+          //console.log("Non split: " + localClassrooms[classroom][continuityTuple])
+          //console.log("Splitted: " + continuity_tuple)
 
-            //Checks if given hour is within continuityTuple
-            if(this.isInCountinuityTuple(hour, continuity_tuple))
-            {
-              //If so, it means that the classroom is occupied, thus, there's no need to check
-              //if it's within another tuple (and continue with the next classroom)
-              warden = false
-              break
-            }
-
-            //Compares actual continuity tuple difference with the actual best aproximation
-            tupleHour = parseInt(continuity_tuple[0])
-            difference = tupleHour - hour
-
-            if(difference > 0 && difference<timeUntilOccupation)
-            {
-              //Due to implementation, base 10 aritmethic will result in unconsistent behaviour
-              //when time comparisons are made between different hours (ie. 09:45 vs 10:10)
-
-              //This will be fixed later using hourI/F
-              timeUntilOccupation = difference
-
-              hourI = Math.floor(hour/100)
-              hourF = Math.floor(tupleHour/100)
-            }
+          //Checks if given hour is within continuityTuple
+          if (this.isInCountinuityTuple(hour, continuity_tuple)) {
+            //If so, it means that the classroom is occupied, thus, there's no need to check
+            //if it's within another tuple (and continue with the next classroom)
+            warden = false;
+            break;
           }
 
-          //If the warden remains true, the hour is not in any tuple of the classrooms
-          //schedules, meaning it's free! (eureka!)
-          if (warden)
-          {
-            //Printing state for testing 
-            /*
+          //Compares actual continuity tuple difference with the actual best aproximation
+          tupleHour = parseInt(continuity_tuple[0]);
+          difference = tupleHour - hour;
+
+          if (difference > 0 && difference < timeUntilOccupation) {
+            //Due to implementation, base 10 aritmethic will result in unconsistent behaviour
+            //when time comparisons are made between different hours (ie. 09:45 vs 10:10)
+
+            //This will be fixed later using hourI/F
+            timeUntilOccupation = difference;
+
+            hourI = Math.floor(hour / 100);
+            hourF = Math.floor(tupleHour / 100);
+          }
+        }
+
+        //If the warden remains true, the hour is not in any tuple of the classrooms
+        //schedules, meaning it's free! (eureka!)
+        if (warden) {
+          //Printing state for testing
+          /*
             console.log("----Start----")
             console.log("hI: " + hourI)
             console.log("hF: " + hourF)
-            */            
+            */
 
-            //Creates an object with classroom id as a key and time until occupation as value
-            if(timeUntilOccupation == 9000)
-            {
-              timeUntilOccupation = "Hasta el final del día"
-              //--
-              classroomObject = {}
-              classroomObject["id"] = classroom
-              classroomObject["TUO"] = timeUntilOccupation
-              //--
-              this.freeClassrooms.push(classroomObject)
-            }
-            
+          //Creates an object with classroom id as a key and time until occupation as value
+          if (timeUntilOccupation == 9000) {
+            timeUntilOccupation = "Hasta el final del día";
+            //--
+            classroomObject = {};
+            classroomObject["id"] = classroom;
+            classroomObject["TUO"] = timeUntilOccupation;
+            //--
+            this.freeClassrooms.push(classroomObject);
+          } else if (hourF - hourI >= 0) {
             //Due to implementation, base 10 aritmethic will result in unconsistent behaviour
             //when a classroom is free for longer than 60 minutes
 
             //Formula obtained by experimental trial and error
-            else if(hourF-hourI >= 0)
-            {
-              //console.log("TUO before: " + timeUntilOccupation)
-              timeUntilOccupation = timeUntilOccupation - (40*(hourF - hourI))
-              //console.log("TUO after: " + timeUntilOccupation)
-              if (timeUntilOccupation >10)
-              {
-                //--
-                classroomObject = {}
-                classroomObject["id"] = classroom
-                classroomObject["TUO"] = timeUntilOccupation
-                //--
-                this.freeClassrooms.push(classroomObject)
-              }
-            }   
+            //console.log("TUO before: " + timeUntilOccupation)
+            timeUntilOccupation = timeUntilOccupation - 40 * (hourF - hourI);
+            //console.log("TUO after: " + timeUntilOccupation)
+            if (timeUntilOccupation > 10) {
+              //--
+              classroomObject = {};
+              classroomObject["id"] = classroom;
+              classroomObject["TUO"] = timeUntilOccupation;
+              //--
+              this.freeClassrooms.push(classroomObject);
+            }
           }
+        }
 
-          //Note that if the class is not free, timeUntilOcupation's value is irrelevant
-          //(for now) [as well with hourI/hourF]
+        //Note that if the class is not free, timeUntilOcupation's value is irrelevant
+        //(for now) [as well with hourI/hourF]
 
-          //----------Creating unique building list---------
-          if(!this.arrContains(this.allBuildings, this.extractBuilding(classroom)))
-              this.allBuildings.push(this.extractBuilding(classroom))
-          //-------------------
+        //----------Creating unique building list---------
+        if (
+          !this.arrContains(this.allBuildings, this.extractBuilding(classroom))
+        )
+          this.allBuildings.push(this.extractBuilding(classroom));
+        //-------------------
       }
     }
   }
